5dafb2b21a4bb1a74057367bc39f494d8f8ddce7
Lucas Maximiliano Perez <maximiliano.lump@gmail.com>
2023-03-14 16:12:49 -0300

    taskVue

--- assets/js/stats.js
+++ assets/js/stats.js
@@ -1,53 +1,82 @@
+ 
+//crea una instancia de Vue
+const { createApp } = Vue;
+
+createApp({
+  //define el estado de la aplicación
+  data() {
+    return {
+      message: "¡Hola, Mundo!",
+      info: null,
+      cardAssistance: [],
+      cardEstimate: [],
+    };
+  },
+      async created  ()  {
+    const date = await fetch("https://mindhub-xj03.onrender.com/api/amazing");
+    const card = await date.json();
+     
+      try{
+        card.events.forEach((element) => {
+          if (element.assistance != undefined) {
+            this.cardAssistance.push(element);
+            //console.log(element.name+" : "+ ((element.assistance/element.capacity)*100).toFixed(2) + " %")
+          } else {
+            //console.log(element.name+" : "+ ((element.estimate/element.capacity)*100).toFixed(2) + " % -" + element.date)
+            this.cardEstimate.push(element);
+          }
+      });
+      this.cardAssistance.sort(function (x, y) {
+          if (x.assistance / x.capacity > y.assistance / y.capacity) {
+            return -1;
+          }
+          if (x.assistance / x.capacity < y.assistance / y.capacity) {
+            return 1;
+          }
+          return 0;
+      });
+      this.cardEstimate.sort(function (x, y) {
+          if (x.capacity > y.capacity) {
+            return -1;
+          }
+          if ( x.capacity < y.capacity) {
+            return 1;
+          }
+          return 0;
+      });
+      }catch(error){
+        console.log(error);
+
+
+      }
+     
+    },
+   
+    methods: {
+      categoryTotal(cat){
+        let total=0;
+        this.cardAssistance.forEach(element => {
+          if(element.category==cat){
+            total
+          }
+          
+        });
+        return total;
+      }
+    },
+  
+   
+
+  //inicializa la instancia de Vue
+}).mount("#app");
+
+  
 
 
 
-stats();
-print();
 
- async function stats() { 
-    try {
-    const date =  await fetch("https://mindhub-xj03.onrender.com/api/amazing")
-        console.log(date);
-        const card = await date.json();
-         card.events.forEach(element => {
-            try{element.assistance
-                 if(element.assistance != undefined) {
-                    console.log(element.name+" : "+ ((element.assistance/element.capacity)*100).toFixed(2) + " %")
-                 }
-                 else {
-                    console.log(element.name+" : "+ ((element.estimate/element.capacity)*100).toFixed(2) + " % -" + element.date)
-                 }
 
-                
 
-            }
-            catch(err){
-                console.log(err);
-            }
 
-        });  
 
-    //   .then((response) =>  response.json() )
-    //   .then(json => console.log(json)  )
-//       .then((json) => {
-//         let cardView2 = json.events.filter(
-//           (card) => dateCreate(card.date) >= dateCreate(json.currentDate)
-//         );
-//         pushCard(cardView2);
-//         cardView2.forEach((element) => {
-//           categoryCreate(element.category);
-//         });
-//         renderCard(cardDate);
-//         categoryView();
-//         iniciarVariables();
-//       });
-//       .catch(err) {
-//         console.log(err);
-       }
-       catch (err) {
-        console.log(err);
-       }
-   } 
-   function print(){
-    console.log("hola perro");
-   }
+ 
